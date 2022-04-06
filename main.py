41cebc33987e36ffeb691a9556f277012af45274
Ishan Karmakar <ishan.karmakar24@gmail.com>
2022-04-06 13:55:07 +0000

    Add postgres code to python

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import psycopg2
+
+con = psycopg2.connect(
+	host = "postgres",
+	port = 5432,
+	database = "postgres",
+	user = "postgres",
+	password = "postgres"
+)
+
+cur = con.cursor()
+cur.execute("SELECT 1")
+res = cur.fetchone()
+print(res)
+
+con.close()
