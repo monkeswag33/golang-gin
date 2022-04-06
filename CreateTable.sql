b19749c4e929c85969107d69f33d941e57bd8b72
Ishan Karmakar <ishan.karmakar24@gmail.com>
2022-04-06 15:54:59 +0000

    Fix Gin returning null for empty table

--- CreateTable.sql
+++ CreateTable.sql
@@ -2,4 +2,6 @@ CREATE TABLE mytable (
 	id SERIAL PRIMARY KEY,
 	firstname TEXT NOT NULL,
 	lastname TEXT NOT NULL
-)
+);
+
+INSERT INTO mytable (firstname, lastname) VALUES ('User1First', 'User1Last'), ('User2First', 'User2Last');
